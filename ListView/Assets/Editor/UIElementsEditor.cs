f46568e5eb3b23a5cbd5d8227440a1465386abae
a4765041 <275442646@qq.com>
2019-07-24 21:45:31 +0800

    添加UI节点关联脚本

---
+++ ListView/Assets/Editor/UIElementsEditor.cs
@@ -0,0 +1,135 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditorInternal;
+
+namespace UGUI.Editor
+{
+    /// <summary>
+    /// 对数据<see cref="UIElements"/>的编辑器绘制
+    /// </summary>
+    [CustomEditor(typeof(UIElements))]
+    public class UIElementsEditor : UnityEditor.Editor
+    {
+        #region Internal Methods
+        private void OnEnable()
+        {
+            var type = target.GetType();
+            var flag = BindingFlags.Instance |
+                       BindingFlags.NonPublic |
+                       BindingFlags.GetField;
+
+            keys = (List<string>)type.InvokeMember("keys", flag, null,
+                target, null);
+            values = (List<Object>)type.InvokeMember("values", flag, null,
+                target, null);
+
+            if (keys == null || values == null)
+            {
+                flag = BindingFlags.Instance |
+                      BindingFlags.NonPublic |
+                      BindingFlags.SetField;
+
+                type.InvokeMember("keys", flag, null,
+                   target, new object[] { new List<string>() });
+
+                type.InvokeMember("values", flag, null,
+                   target, new object[] { new List<Object>() });
+            }
+
+            list = new ReorderableList(keys, typeof(string));
+            list.drawHeaderCallback = DrawHeaderCallback;
+            list.drawElementCallback = DrawElementCallback;
+            list.onAddCallback = OnAddCallback;
+            list.onRemoveCallback = OnRemoveCallback;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.Space();
+            serializedObject.Update();
+            list.DoLayoutList();
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void DrawHeaderCallback(Rect rect)
+        {
+            GUI.Label(rect, "Targets");
+
+            if (!rect.Contains(Event.current.mousePosition))
+            {
+                return;
+            }
+
+            if (Event.current.type == EventType.DragUpdated)
+            {
+                DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+                Event.current.Use();
+            }
+            else if (Event.current.type == EventType.DragPerform)
+            {
+                DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+                Event.current.Use();
+                if (DragAndDrop.objectReferences.Length > 0)
+                {
+                    var obj = DragAndDrop.objectReferences[0];
+                    Undo.RegisterCompleteObjectUndo(target, "addDragItem");
+                    keys.Add(obj.name);
+                    values.Add(obj);
+                    EditorUtility.SetDirty(target);
+                }
+            }
+        }
+
+        private void OnRemoveCallback(ReorderableList reorderAbleList)
+        {
+            Undo.RegisterCompleteObjectUndo(target, "deleteItem");
+            keys.RemoveAt(reorderAbleList.index);
+            values.RemoveAt(reorderAbleList.index);
+            EditorUtility.SetDirty(target);
+            reorderAbleList.index = keys.Count - 1;
+        }
+
+        private void OnAddCallback(ReorderableList reorderAbleList)
+        {
+            Undo.RegisterCompleteObjectUndo(target, "addItem");
+            keys.Add("Unique");
+            values.Add(null);
+            EditorUtility.SetDirty(target);
+        }
+
+        private void DrawElementCallback(Rect rect, int index, bool isActive, bool isFocused)
+        {
+            Rect labelRect = rect;
+            labelRect.xMax = EditorGUIUtility.labelWidth - 10;
+
+            EditorGUI.BeginChangeCheck();
+            string newName = EditorGUI.TextField(labelRect, keys[index]);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RegisterCompleteObjectUndo(target, "modifyItemKey");
+                keys[index] = newName;
+                EditorUtility.SetDirty(target);
+            }
+
+            Rect valueRect = rect;
+            valueRect.xMin = labelRect.xMax;
+            EditorGUI.BeginChangeCheck();
+            var newValue = EditorGUI.ObjectField(valueRect, values[index], typeof(Object), true);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RegisterCompleteObjectUndo(target, "modifyItemValue");
+                values[index] = newValue;
+                EditorUtility.SetDirty(target);
+            }
+        }
+        #endregion
+
+        #region Internal Fields
+        private ReorderableList list;
+        private List<string> keys;
+        private List<Object> values;
+        #endregion
+    }
+}
---
+++ ListView/Assets/Scripts/UIElements.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+
+namespace UGUI
+{
+    /// <summary>
+    /// 可配置的UI节点关联脚本
+    /// </summary>
+    public class UIElements : MonoBehaviour
+    {
+        #region Public Methods
+        /// <summary>
+        /// 获取关联数据
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="name">名字</param>
+        /// <returns>返回关联数据</returns>
+        public T GetObject<T>(string name) where T : UObject
+        {
+            if (keys == null || values == null)
+            {
+                return null;
+            }
+
+            int count = keys.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var key = keys[i];
+                if (name == key)
+                {
+                    var result = values[i] as T;
+                    if (result != null)
+                    {
+                        return result;
+                    }
+
+                    var go = values[i] as GameObject;
+
+                    return go.GetComponent<T>();
+                }
+            }
+
+            return default(T);
+        }
+        #endregion
+
+        #region Internal Fields
+        [SerializeField]
+        private List<string> keys = null;
+        [SerializeField]
+        private List<UObject> values = null;
+        #endregion
+    }
+}
